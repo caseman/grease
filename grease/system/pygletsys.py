3fae3b82464afd3b177592cd8bd594a529317d87
casey.duncan <casey.duncan@695b90bb-9647-0410-898f-0949cf7e6fe8>
2010-01-17 06:52:43 +0000

    Add exciting copyright
    git-svn-id: https://caseman.googlecode.com/svn/trunk/grease@538 695b90bb-9647-0410-898f-0949cf7e6fe8

--- grease/system/pygletsys.py
+++ grease/system/pygletsys.py
@@ -1,3 +1,15 @@
+#############################################################################
+#
+# Copyright (c) 2010 by Casey Duncan
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the MIT License
+# A copy of the license should accompany this distribution.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#
+#############################################################################
 """Grease systems specific to Pyglet"""
 
 import new
