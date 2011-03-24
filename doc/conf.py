ec18dde6e0a71a086a93e532871d452551371962
Casey Duncan <casey.duncan@gmail.com>
2011-03-23 17:59:27 -0700

    Add sphinx.ext.viewcode for awesome src code links from refdocs

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
 
 autodoc_member_order = 'groupwise'
 
